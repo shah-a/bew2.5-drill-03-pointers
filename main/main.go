925e744ee802e9000fdb5e6ce0070b4febf5f4ae
Ali Shah <s.ashah@outlook.com>
2021-06-28 19:33:02 -0400

    [Finished] drill Q5 challenge

--- main/main.go
+++ main/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
+
+func swap(x, y *int) {
+	*x, *y = *y, *x
+}
 
 func main() {
-	fmt.Println("Salaam, world!")
+	var x, y int
+
+	fmt.Print("Enter integer #1 -> ")
+	if _, err := fmt.Scanln(&x); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Enter integer #2 -> ")
+	if _, err := fmt.Scanln(&y); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println() // For terminal spacing
+
+	swap(&x, &y)
+
+	fmt.Println("Your swapped values are:")
+	fmt.Printf("x -> %d\ny -> %d\n", x, y)
 }
